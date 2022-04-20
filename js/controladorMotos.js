91be9edf61031ec811830b265d49aaa53e6c40ff
santiagoft19 <floreztamayosantiago@gmail.com>
2022-04-19 22:21:47 -0500

    Se organiza moto yamaha

---
+++ js/controladorMotos.js
@@ -0,0 +1,19 @@
+function llenarMotos(){
+
+    let yamaha=[
+        {nombre:"Dt 125",precio:7000000,modelo:"2006",foto:"../img/Yamaha/dt-125.jpg"},
+        {nombre:"Calimatic 175",precio:10000000,modelo:"1998",foto:"../img/Yamaha/calimatic.jpg"},
+        {nombre:"Dt 175",precio:11000000,modelo:"2003",foto:"../img/Yamaha/dt-175.jpg"},
+        {nombre:"Dt 200",precio:18000000,modelo:"2001",foto:"../img/Yamaha/dt-200.jpg"},
+        {nombre:"Rx 100",precio:7000000,modelo:"2006",foto:"../img/Yamaha/Rx100.jpg"},
+        {nombre:"Rx 115",precio:8000000,modelo:"2008",foto:"../img/Yamaha/rx-115.jpg"},
+        {nombre:"Rx 135",precio:20000000,modelo:"1999",foto:"../img/Yamaha/rx-135.jpg"},
+        {nombre:"Rx-z",precio:20000000,modelo:"1999",foto:"../img/Yamaha/rx-z.jpg"},
+        {nombre:"V 80",precio:5000000,modelo:"2000",foto:"../img/Yamaha/v-80.jpg"},
+        {nombre:"Yz 85",precio:14000000,modelo:"2006",foto:"../img/Yamaha/yz-85.jpg"},
+        {nombre:"Yz 125",precio:21000000,modelo:"2011",foto:"../img/Yamaha/yz-125.jpg"},
+        {nombre:"Yz 250",precio:24000000,modelo:"2010",foto:"../img/Yamaha/yz-250.jpg"},
+        {nombre:"Yz 450",precio:30000000,modelo:"2006",foto:"../img/Yamaha/yz-450.jpg"},
+        {nombre:"Xt 500",precio:25000000,modelo:"1980",foto:"../img/Yamaha/Yamaha-Xt-500-.jpg"}
+    ]
+}
